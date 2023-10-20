0b193795280e089b44dfeaae46184a203b89ad52
KolosIryna <127253667+KolosIryna@users.noreply.github.com>
2023-10-20 23:02:45 +0200

    commit

--- src/components/MoviesItem/MoviesItem.jsx
+++ src/components/MoviesItem/MoviesItem.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 import { StyledMovieItem } from './MoviesItem.styled';
 
-const MoviesItem = ({ id, poster_path, title }) => {
+const MoviesItem = ({ id, image, title }) => {
   const defaultImg =
     'https://ireland.apollo.olxcdn.com/v1/files/0iq0gb9ppip8-UA/image;s=1000x700';
 
@@ -11,13 +11,10 @@ const MoviesItem = ({ id, poster_path, title }) => {
     <StyledMovieItem>
       <Link to={`/movies/${id}`} className="link">
         <img
-          src={
-            poster_path
-              ? `https://image.tmdb.org/t/p/w500/${poster_path}`
-              : defaultImg
-          }
-          width={250}
+          src={image ? `https://image.tmdb.org/t/p/w300/${image}` : defaultImg}
+          width={300}
           alt="poster"
+          className="image"
         />
         <h2 className="title">{title}</h2>
       </Link>
